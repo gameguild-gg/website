f2e584c19a72d9ceca615db04fbd1488495a34c8
Germano <germano-software-dev@hotmail.com>
2023-05-31 16:31:58 -0300

    feat(Post): Add base module.

--- src/modules/post/post.module.ts
+++ src/modules/post/post.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PostService } from './post.service';
+import { PostController } from './post.controller';
 
-@Module({})
+@Module({
+  providers: [PostService],
+  controllers: [PostController]
+})
 export class PostModule {}
