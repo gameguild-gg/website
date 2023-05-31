5a529d3d49f68f938472c8a7735069571cb0d1ca
Germano <germano-software-dev@hotmail.com>
2023-05-31 16:25:14 -0300

    feat(Event): Add base module.

--- src/modules/event/event.controller.ts
+++ src/modules/event/event.controller.ts
@@ -1,4 +1,37 @@
 import { Controller } from '@nestjs/common';
+import { EventService } from './event.service';
+import { Crud, CrudController } from '@dataui/crud';
+import { EventEntity } from './event.entity';
+import { ApiTags } from '@nestjs/swagger';
+import { EEventType } from '../../constants/event.enum';
+import { ProposalCategory } from '../../constants/proposal.enum';
 
+@Crud({
+    model: {
+        type: EventEntity,
+    },
+    params: {
+        id: {
+            field: 'id',
+            type: 'uuid',
+            primary: true,
+        },
+        type: {
+            field: 'type',
+            enum: EEventType,
+        },
+        category: {
+            field: 'category',
+            enum: ProposalCategory,
+        }
+    }
+})
 @Controller('event')
-export class EventController {}
+@ApiTags('event')
+export class EventController implements CrudController<EventEntity> {
+    constructor(public service: EventService) {}
+
+    get base(): CrudController<EventEntity> {
+        return this;
+    }
+}
--- src/modules/event/event.module.ts
+++ src/modules/event/event.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EventService } from './event.service';
 import { EventController } from './event.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EventEntity } from './event.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([EventEntity])],
   providers: [EventService],
   controllers: [EventController]
 })
