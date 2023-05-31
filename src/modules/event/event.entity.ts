0c4e1cea67ab8658e944c8c7b74ffb9d4d0e9691
Germano <germano-software-dev@hotmail.com>
2023-05-31 16:40:19 -0300

    feat: Add base entities for Event and User.

--- src/modules/event/event.entity.ts
+++ src/modules/event/event.entity.ts
@@ -1,5 +1,23 @@
-import { EntityBase } from "../../common/entity.base";
-import { Entity } from "typeorm";
+import { EntityBase } from '../../common/entity.base';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { UserEntity } from '../user/user.entity';
+import { EEventType } from '../../constants/event.enum';
 
 @Entity({ name: 'event' })
-export class EventEntity extends EntityBase {}
+export class EventEntity extends EntityBase {
+    @Column()
+    title: string;
+
+    @Column()
+    day: Date;
+
+    @Column()
+    time: Date;
+
+    @Column()
+    type: EEventType;
+
+    // relations
+    @ManyToOne(() => UserEntity, (user) => user.events)
+    creator: UserEntity;
+}
--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,5 +1,36 @@
-import { EntityBase } from "../../common/entity.base";
-import { Entity } from "typeorm";
+import { EntityBase } from '../../common/entity.base';
+import { Column, Entity, OneToMany } from 'typeorm';
+import { EventEntity } from '../event/event.entity';
+import { ERole } from '../../constants/role.enum';
+import { CourseEntity } from '../course/course.entity';
 
 @Entity({ name: 'user' })
-export class UserEntity extends EntityBase {}
+export class UserEntity extends EntityBase {
+    @Column()
+    username: string;
+    
+    @Column()
+    firstname: string;
+    
+    @Column()
+    lastname: string;
+    
+    @Column()
+    email: string;
+    
+    @Column()
+    cpf: string;
+    
+    @Column()
+    role: ERole;
+    
+    @Column()
+    active: boolean;
+
+    // relations
+    @OneToMany(() => EventEntity, (event) => event.id)
+    events: EventEntity;
+    
+    // @OneToMany(() => CourseEntity, (course) => course.id)
+    // courses: CourseEntity;
+}
