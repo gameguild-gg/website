12d7126cb6458047fcbea7e8d9d00cde837d4cb6
Germano <germano-software-dev@hotmail.com>
2023-05-31 16:08:52 -0300

    feat(Event): Add base module.

--- src/modules/event/event.module.ts
+++ src/modules/event/event.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { EventService } from './event.service';
+import { EventController } from './event.controller';
 
-@Module({})
+@Module({
+  providers: [EventService],
+  controllers: [EventController]
+})
 export class EventModule {}
