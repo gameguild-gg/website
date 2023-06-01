5ec98f8f49fbb05a1fcbc6f81c8c300b5740f687
Germano <germano-software-dev@hotmail.com>
2023-06-01 12:44:45 -0300

    fix: Front base layout.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
