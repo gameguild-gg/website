291a563550faa130d1c45a61e935ac4e9e89db24
Alec Santos <alecsantos96@gmail.com>
2023-10-02 10:31:03 -0300

    Habilitar recurso da pasta app do NextJS

--- front/next.config.js
+++ front/next.config.js
@@ -15,4 +15,7 @@ module.exports = withBundleAnalyzer({
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
 });
