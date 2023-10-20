3046a97ed0be151448375efadc3101d3f61b4d86
Alexandre Tolstenko <tolstenko@gmail.com>
2023-10-20 12:50:54 -0400

    fix(multer): fix multer upload

--- back/src/modules/competition/dtos/competition.submission.dto.ts
+++ back/src/modules/competition/dtos/competition.submission.dto.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
+// This is a hack to make Multer available in the Express namespace
+import 'multer'; // a hack to make Multer available in the Express namespace
 
 export class CompetitionSubmissionDto {
   @ApiProperty({ required: true })
