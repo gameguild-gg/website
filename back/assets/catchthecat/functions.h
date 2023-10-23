65f215a8792b35eee4d633eeb4aca8b9eb2bba38
Alexandre Tolstenko <tolstenko@gmail.com>
2023-10-23 08:33:59 -0400

    fix(cmake): fix cmake version

--- back/assets/catchthecat/functions.h
+++ back/assets/catchthecat/functions.h
@@ -33,35 +33,6 @@ int Range(int start, int end) {
     return dist(rng);
 }
 
-struct Point2D {
-    int32_t x, y;
-    Point2D(int32_t x, int32_t y): x(x), y(y) {}
-    // for unordered_set unordered_map
-    uint64_t hash() const noexcept { return ((uint64_t)x) << 32 | (uint64_t)y; }
-    // for priority_queue, map, set
-    bool operator < (const Point2D& p) const{ return x < p.x || (x == p.x && y < p.y); }
-    // converts pair<int, int> to Point2D
-    Point2D(const pair<int32_t, int32_t>& p): x(p.first), y(p.second) {}
-    // converts Point2D to pair<int, int>
-    std::pair<int,int> toPair(){return {x,y};}
-    // add other operators, acessors, etc. ex.: NE(), NW(), SE(), SW(), E(), W()
-};
-// tell the compiler that we want to use Point2D as a key in unordered_set and unordered_map
-template <> struct std::hash<Point2D> {
-    std::size_t operator()(const Point2D& p) const noexcept { return p.hash(); }
-};
-
-struct AStarNode {
-    Point2D pos;
-    float accumulatedCost;
-    float heuristic;
-    // for priority_queue
-    bool operator < (const AStarNode& n) const{
-        return this->accumulatedCost + this->heuristic < n.accumulatedCost + n.heuristic;
-    }
-};
-
-
 struct Position {
     int32_t x;
     int32_t y;
@@ -133,16 +104,11 @@ struct AStarNode {
     Position pos;
     float accumulatedCost;
     float heuristic;
+    // for priority_queue
     bool operator < (const AStarNode& n) const{
         return this->accumulatedCost + this->heuristic < n.accumulatedCost + n.heuristic;
     }
 };
-
-void test(){
-    priority_queue<AStarNode> frontier;
-    frontier.push({{0,0}, 0, 0});
-}
-
 
 struct Board {
     vector<bool> blocked;
@@ -250,6 +216,7 @@ bool CatcherWon(const Board& board, const Position& catPos) {
     return true;
 }
 
+// BFS
 vector<Position> buildPath(Board& board){
     auto cat = board.catPos;
     vector<Position> path;
