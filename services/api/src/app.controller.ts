22bc43659ec257177a7fd0a3d65dfcc207d4a211
Alexandre Tolstenko <tolstenko@gmail.com>
2024-02-05 15:58:08 -0500

    fix: documentation redirect

--- services/api/src/app.controller.ts
+++ services/api/src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
+import { Public } from "./auth";
 
 @Controller()
 export class AppController {
   @Get()
   @Redirect('/documentation', 302)
   @ApiExcludeEndpoint()
+  @Public()
   root() {}
 }
