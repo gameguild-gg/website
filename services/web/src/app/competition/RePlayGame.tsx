54abcc1b6b779958f939be87acbb29d73aa86fca
Alexandre Tolstenko <tolstenko@gmail.com>
2024-02-27 20:30:57 -0500

    feat: replay match

--- services/web/src/app/competition/RePlayGame.tsx
+++ services/web/src/app/competition/RePlayGame.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import { Chessboard } from 'react-chessboard';
-import { Chess } from 'chess.js';
 import { getCookie } from 'cookies-next';
 import { useRouter } from 'next/navigation';
 import { ChessMatchResultDto } from '@game-guild/common/src/competition/chess-match-result.dto';
+import { Button, message, Space, Typography } from 'antd';
+import { Chess, Move, WHITE } from 'chess.js';
 
 // WATCH A REPLAY OF A GAME
 // IT NEED TO RECEIVE THE WHOLE HISTORY OF THE GAME OR RECEIVE MATCH ID AND GET THE HISTORY FROM THE SERVER
@@ -11,6 +12,10 @@ import { ChessMatchResultDto } from '@game-guild/common/src/competition/chess-ma
 const RePlayGame: React.FC = () => {
   const queryParameters = new URLSearchParams(window.location.search);
   const matchId = queryParameters.get('matchId');
+  const [states, setStates] = React.useState<string[]>([]);
+  const [currentStateId, setCurrentStateId] = React.useState<number>(0);
+
+  const chess: Chess = new Chess();
 
   const router = useRouter();
 
@@ -43,6 +48,16 @@ const RePlayGame: React.FC = () => {
     console.log(data);
     setMatchData(data);
     setMatchFetched(true);
+
+    // Load the moves
+    const list: string[] = [];
+    list.push('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
+    data.moves.forEach((move) => {
+      chess.move(move);
+      list.push(chess.fen());
+    });
+    setStates(list);
+    message.info('Match data fetched');
   }
 
   useEffect(() => {
@@ -52,25 +67,37 @@ const RePlayGame: React.FC = () => {
   });
 
   return (
-    <>
-      <p>
-        {' '}
-        Todo: create ways to go back and forth in the game using the moves
-        array.{' '}
-      </p>
-      <p>Match ID: {matchId}</p>
-      <p>
-        {matchData && matchData.result}
-        <br />
-        {matchData && matchData.players.map((player) => player + ' ')}
-        <br />
-        {matchData && matchData.winner}
-        <br />
-        {matchData && matchData.moves.map((move) => move + ' ')}
-        <br />
-        {matchData && matchData.finalFen}
-      </p>
-    </>
+    <Space direction={'vertical'}>
+      <Typography.Title level={1}>Replay Game</Typography.Title>
+      <Typography.Paragraph>
+        {matchData?.players[1] + ' vs ' + matchData?.players[0]}
+      </Typography.Paragraph>
+      <Chessboard
+        position={states[currentStateId]}
+        boardWidth={400}
+        arePiecesDraggable={false}
+      />
+      <Space direction={'horizontal'}>
+        <Button
+          onClick={() => {
+            if (currentStateId > 0) {
+              setCurrentStateId(currentStateId - 1);
+            }
+          }}
+        >
+          Previous
+        </Button>
+        <Button
+          onClick={() => {
+            if (currentStateId < states.length - 1) {
+              setCurrentStateId(currentStateId + 1);
+            }
+          }}
+        >
+          Next
+        </Button>
+      </Space>
+    </Space>
   );
 };
 
