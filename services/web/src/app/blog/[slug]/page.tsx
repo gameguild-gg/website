c60c366b71631942887f1ff02468e9cc570e51ee
Alexandre Tolstenko <tolstenko@gmail.com>
2024-03-08 10:32:18 -0500

    fix: compilation issues related to typing

--- services/web/src/app/blog/[slug]/page.tsx
+++ services/web/src/app/blog/[slug]/page.tsx
@@ -3,24 +3,22 @@
 import { useRouter, useParams } from 'next/navigation';
 import { notification, NotificationArgsProps } from 'antd';
 import React, { useEffect, useState } from 'react';
-// @ts-ignore
-import GhostContentAPI from '@tryghost/content-api'
+import GhostContentAPI, { PostOrPage } from '@tryghost/content-api';
 //import { NotificationProvider } from '@/app/NotificationContext';
 //import { UserOutlined } from '@ant-design/icons';
 
-
 function Blog() {
   const [api, contextHolder] = notification.useNotification();
-  const [post, setPost] = useState<any>()
+  const [post, setPost] = useState<PostOrPage>();
   const router = useRouter();
-  const params = useParams()
+  const params = useParams();
 
   const ghost = new GhostContentAPI({
     url: 'https://gameguild.gg',
-    key: process.env.NEXT_PUBLIC_GHOST_CONTENT_API_KEY,
-    version: "v5.0"
+    key: process.env.NEXT_PUBLIC_GHOST_CONTENT_API_KEY ?? '',
+    version: 'v5.0',
   });
-  
+
   const openNotification = (
     description: string,
     message: string = 'info',
@@ -33,15 +31,16 @@ function Blog() {
     });
   };
 
-  useEffect( () => {
-    console.log("useEffect()")
-    console.log("slug: ",params.slug)
+  useEffect(() => {
+    console.log('useEffect()');
+    console.log('slug: ', params.slug);
     async function fetchMyAPI() {
-      setPost(await ghost.posts.read({slug: params.slug}, {formats: ['html']})) //, {include: 'tags,authors'}
+      const slug: string = params.slug as string;
+      setPost(await ghost.posts.read({ slug }, { formats: ['html'] })); //, {include: 'tags,authors'}
     }
-    fetchMyAPI()
-    console.log(post)
-  },[]);
+    fetchMyAPI();
+    console.log(post);
+  }, []);
 
   return (
     <>
@@ -53,7 +52,7 @@ function Blog() {
           alignItems: 'center',
           alignContent: 'center',
           overflow: 'hidden',
-          width: '100%'
+          width: '100%',
         }}
       >
         <div
@@ -62,19 +61,27 @@ function Blog() {
             width: '100%',
             background: '#18181c',
           }}
-          className='mx-auto mh-full'
+          className="mx-auto mh-full"
         >
-          {post &&
-            <div className='mx-[10px]'>
-              <br/>
-              <div className='text-5xl'>{post.title}</div>
-              <br/>
-              <img src={post.feature_image} className='object-cover w-[1200px]'/>
-              <br/>
-              <div className='text-left' dangerouslySetInnerHTML={{__html: post.html}} />
+          {post && (
+            <div className="mx-[10px]">
+              <br />
+              <div className="text-5xl">{post.title}</div>
+              <br />
+              {post.feature_image && (
+                <img
+                  src={post.feature_image as string}
+                  className="object-cover w-[1200px]"
+                  alt={post.feature_image_alt as string}
+                />
+              )}
+              <br />
+              <div
+                className="text-left"
+                dangerouslySetInnerHTML={{ __html: post?.html as string }}
+              />
             </div>
-          }
-          
+          )}
         </div>
       </div>
     </>
--- services/web/src/app/blog/page.tsx
+++ services/web/src/app/blog/page.tsx
@@ -3,24 +3,21 @@
 import { useRouter } from 'next/navigation';
 import { notification, NotificationArgsProps } from 'antd';
 import React, { useEffect, useState } from 'react';
-// @ts-ignore
-import GhostContentAPI from '@tryghost/content-api'
+import GhostContentAPI from '@tryghost/content-api';
 //import { NotificationProvider } from '@/app/NotificationContext';
 //import { UserOutlined } from '@ant-design/icons';
 
-
-
 function Blog() {
   const [api, contextHolder] = notification.useNotification();
-  const [posts, setPosts] = useState<any[]>([])
+  const [posts, setPosts] = useState<any[]>([]);
   const router = useRouter();
 
   const ghost = new GhostContentAPI({
     url: 'https://gameguild.gg',
-    key: process.env.NEXT_PUBLIC_GHOST_CONTENT_API_KEY,
-    version: "v5.0"
+    key: process.env.NEXT_PUBLIC_GHOST_CONTENT_API_KEY ?? '',
+    version: 'v5.0',
   });
-  
+
   const openNotification = (
     description: string,
     message: string = 'info',
@@ -33,18 +30,18 @@ function Blog() {
     });
   };
 
-  useEffect( () => {
-    console.log("useEffect()")
+  useEffect(() => {
+    console.log('useEffect()');
     async function fetchMyAPI() {
-      setPosts(await ghost.posts.browse({include: 'tags,authors'})) //pega os 15 primeiros por padrão.
+      setPosts(await ghost.posts.browse({ include: ['tags', 'authors'] })); //pega os 15 primeiros por padrão.
     }
-    fetchMyAPI()
-    console.log(posts)
-  },[]);
+    fetchMyAPI();
+    console.log(posts);
+  }, []);
 
-  const onBlogClick = (slug:string) => {
-    router.push('/blog/'+slug);
-  }
+  const onBlogClick = (slug: string) => {
+    router.push('/blog/' + slug);
+  };
 
   return (
     <div>
@@ -56,34 +53,50 @@ function Blog() {
           alignItems: 'center',
           alignContent: 'center',
           overflow: 'hidden',
-          width: '100%'
+          width: '100%',
         }}
       >
         <div
           style={{
             maxWidth: '1200px',
             width: '100%',
-            background: '#18181c',//#18181c
+            background: '#18181c', //#18181c
           }}
-          className='grid grid-cols-3 mx-auto'
+          className="grid grid-cols-3 mx-auto"
         >
-          {posts && posts.map((post:any) =>
+          {posts &&
+            posts.map((post: any) => (
+              <div
+                className="p-[10px] align-top overflow-hidden text-white text-left"
+                key={post.id}
+                onClick={() => onBlogClick(post.slug)}
+              >
+                <img
+                  src={post.feature_image}
+                  className="object-cover  w-[380px] h-[253px]"
+                />
 
-            <div className='p-[10px] align-top overflow-hidden text-white text-left' key={post.id} onClick={() => onBlogClick(post.slug)}>
-              <img src={post.feature_image} className='object-cover  w-[380px] h-[253px]'/>
+                {post.tags &&
+                  post.tags.map((tag: any) => (
+                    <div
+                      className="text-neutral-500 p-1 py-0 mr-1 mt-2 ml-[-4px]"
+                      key={tag.id}
+                    >
+                      {tag.name}
+                    </div>
+                  ))}
 
-              {post.tags && post.tags.map((tag:any)=>
-                <div className='text-neutral-500 p-1 py-0 mr-1 mt-2 ml-[-4px]' key={tag.id}>{tag.name}</div>
-              )}
-
-              <div className='text-2xl'>{post.title}</div>
-              <br />
-              <div>{post.custom_excerpt || post.excerpt}</div>
-              <div className='text-neutral-500 p-1 py-0 ml-[-4px] pt-2'>{new Date(post.published_at).toLocaleDateString('pt-BR',{dateStyle:'short'})} • {post.reading_time} min. de leitura</div>
-            </div>
-
-          )}
-          
+                <div className="text-2xl">{post.title}</div>
+                <br />
+                <div>{post.custom_excerpt || post.excerpt}</div>
+                <div className="text-neutral-500 p-1 py-0 ml-[-4px] pt-2">
+                  {new Date(post.published_at).toLocaleDateString('pt-BR', {
+                    dateStyle: 'short',
+                  })}{' '}
+                  • {post.reading_time} min. de leitura
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     </div>
