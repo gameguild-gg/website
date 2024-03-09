726ffcad89bceffbb15049a781e09693d7fa8597
Matheus Martins <contact@matheusmartins.com>
2024-03-09 20:35:42 -0300

    fix: fetch posts and pagination.

--- services/web/src/actions/blog.ts
+++ services/web/src/actions/blog.ts
@@ -1,26 +1,26 @@
-'use server';
+"use server";
 
-export const fetchPosts = async () => {
+export const fetchPosts = async (page: number = 1, limit: string = "15") => {
   const admin_domain = process.env.GHOST_ADMIN_DOMAIN;
   const content_api_key = process.env.GHOST_CONTENT_API_KEY;
 
   const response = await fetch(
-    `https://${admin_domain}/ghost/api/content/posts?key=${content_api_key}`,
+    `https://${admin_domain}/ghost/api/content/posts?key=${content_api_key}${page > 1 ? `&page=${page}` : ""}${limit != '15' ? `&limit=${limit}` : ""}`,
     {
       headers: {
-        'Content-Type': 'application/json',
-        'Accept-Version': 'v5.0',
-      },
-    },
+        "Content-Type": "application/json",
+        "Accept-Version": "v5.0"
+      }
+    }
   );
 
   if (!response.ok) {
-    throw new Error('Failed to fetch posts');
+    throw new Error("Failed to fetch posts");
   }
 
-  const { posts } = await response.json();
+  const { posts, meta: { pagination } } = await response.json();
 
-  return posts;
+  return { posts, pagination };
 };
 
 export const fetchPost = async (slug: string) => {
@@ -31,14 +31,14 @@ export const fetchPost = async (slug: string) => {
     `https://${admin_domain}/ghost/api/content/posts/slug/${slug}?key=${content_api_key}`,
     {
       headers: {
-        'Content-Type': 'application/json',
-        'Accept-Version': 'v5.0',
-      },
-    },
+        "Content-Type": "application/json",
+        "Accept-Version": "v5.0"
+      }
+    }
   );
 
   if (!response.ok) {
-    throw new Error('Failed to fetch post');
+    throw new Error("Failed to fetch post");
   }
 
   const { posts } = await response.json();
--- services/web/src/app/blog/page.tsx
+++ services/web/src/app/blog/page.tsx
@@ -1,33 +1,50 @@
-import React from 'react';
-import { fetchPosts } from '@/actions/blog';
-import { PostCard } from '@/components/blog/post-card';
+import React from "react";
+import { fetchPosts } from "@/actions/blog";
+import { PostCard } from "@/components/blog/post-card";
+import Link from "next/link";
 
-async function Blog() {
-  const posts = await fetchPosts();
+type Props = {
+  searchParams: {
+    page: number;
+  };
+};
+
+async function Blog({ searchParams: { page } }: Readonly<Props>) {
+  const { posts, pagination } = await fetchPosts(page);
+
+  // TODO: Should move to a BlogPagination component.
+  const pages = [];
+  for (let i = 1; i <= pagination.pages; i++) {
+    pages.push(<Link href={i == 1 ? "/blog" : `/blog?page=${i}`} key={i}>{i}</Link>);
+  }
 
   return (
     <div>
       <div
         style={{
-          textAlign: 'center',
-          color: '#fff',
-          backgroundColor: '#101014',
-          alignItems: 'center',
-          alignContent: 'center',
-          overflow: 'hidden',
-          width: '100%',
+          textAlign: "center",
+          color: "#fff",
+          backgroundColor: "#101014",
+          alignItems: "center",
+          alignContent: "center",
+          overflow: "hidden",
+          width: "100%"
         }}
       >
         <div
           style={{
-            maxWidth: '1200px',
-            width: '100%',
-            background: '#18181c', //#18181c
+            maxWidth: "1200px",
+            width: "100%",
+            background: "#18181c" //#18181c
           }}
           className="grid grid-cols-1 md:grid-cols-3 mx-auto"
         >
           {posts &&
             posts.map((post: any) => <PostCard post={post} key={post.id} />)}
+        </div>
+        <div>
+          {/* TODO: Should be a BlogPagination Component!*/}
+          {pages}
         </div>
       </div>
     </div>
