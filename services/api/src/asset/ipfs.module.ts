80a951be75e35937dfaa446bdfe64211a5b73b08
Alexandre Tolstenko <tolstenko@gmail.com>
2024-03-12 23:54:58 -0400

    fix: module import

--- services/api/src/asset/ipfs.module.ts
+++ services/api/src/asset/ipfs.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { IpfsService } from './ipfs.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { IpfsEntity } from "./ipfs.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([IpfsEntity])],
   providers: [IpfsService],
   exports: [IpfsService]
 })
