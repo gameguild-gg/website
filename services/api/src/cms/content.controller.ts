566bf0de2bf29250e7144cdbe644d019b2dc830f
Alexandre Tolstenko <tolstenko@gmail.com>
2024-03-27 18:29:49 -0400

    fix: add new api for course creation

--- services/api/src/cms/content.controller.ts
+++ services/api/src/cms/content.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Logger, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { ContentService } from './content.service';
-import { CreateCourseDto } from "../dtos/course/create-course.dto";
+import { CreateCourseDto } from '../dtos/course/create-course.dto';
+import { AuthUser } from '../auth';
+import { Auth } from '../auth/decorators/http.decorator';
+import { UserEntity } from '../user/entities';
+import { CourseEntity } from './entities/course.entity';
 
 @Controller('content')
 @ApiTags('content')
@@ -10,6 +14,12 @@ export class ContentController {
 
   constructor(private readonly courseService: ContentService) {}
 
-  @Post()
-  public async create(data: CreateCourseDto) {}
+  @Post('course/create')
+  @Auth()
+  @ApiOkResponse({ type: CourseEntity })
+  public async createEmptyCourse(
+    @AuthUser() user: UserEntity,
+  ): Promise<CourseEntity> {
+    return this.courseService.createEmptyCourse(user);
+  }
 }
