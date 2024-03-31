6297b55af89177a3cb60d9cabe841f60f6785044
Matheus Martins <contact@matheusmartins.com>
2024-03-31 14:51:41 -0300

    fix: removed modals and updated the sign-in and sign-up page

--- services/web/src/app/[locale]/@modal/(auth)/(.)forgot-password/page.tsx
+++
@@ -1,9 +0,0 @@
-import ForgotPasswordForm from "@/components/auth/forgot-password-form";
-
-export default function ForgotPassword() {
-  return (
-    <div>
-      <ForgotPasswordForm />
-    </div>
-  );
-}
