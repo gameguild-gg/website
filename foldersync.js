05d18b2f3662e79177e17456a3e31e69e34d320d
Alexandre Tolstenko <tolstenko@gmail.com>
2024-04-03 20:24:24 -0400

    fix foldersync for windows

--- foldersync.js
+++ foldersync.js
@@ -1,6 +1,6 @@
 // require fs and path
 const fs = require("fs");
-const { join } = require("path");
+const path = require("path");
 
 // get the parameters
 const args = process.argv.slice(2);
@@ -25,11 +25,11 @@ function getAllFiles(dir) {
   const files = [];
   const list = fs.readdirSync(dir, { withFileTypes: true });
   for (const file of list) {
-    const path = join(dir, file.name);
+    const filePath = path.join(dir, file.name);
     if (file.isDirectory()) {
-      files.push(...getAllFiles(path));
+      files.push(...getAllFiles(filePath));
     } else {
-      files.push(path);
+      files.push(filePath);
     }
   }
   return files;
@@ -43,10 +43,10 @@ let files2 = getAllFiles(dir2);
 let filesFiltered1 = [];
 let filesFiltered2 = [];
 files1.forEach(file => {
-  filesFiltered1.push(file.replace(dir1, ""));
+  filesFiltered1.push(path.relative(dir1, file));
 });
 files2.forEach(file => {
-  filesFiltered2.push(file.replace(dir2, ""));
+  filesFiltered2.push(path.relative(dir2, file));
 });
 
 files1 = filesFiltered1;
@@ -57,46 +57,46 @@ const onlyIn1 = files1.filter(f => !files2.includes(f));
 const onlyIn2 = files2.filter(f => !files1.includes(f));
 const common = files1.filter(f => files2.includes(f));
 
-// function to get the dirname
-function dirname(path) {
-  return path.split("/").slice(0, -1).join("/");
+function mkdirSyncRecursive(dir) {
+  const parts = dir.split(path.sep);
+  for (let i = 1; i <= parts.length; i++) {
+    const subPath = path.join(...parts.slice(0, i));
+    if (!fs.existsSync(subPath)) {
+      fs.mkdirSync(subPath);
+    }
+  }
 }
 
 // copy onlyIn1 to dir2, and ensure that the destination directories exist
 onlyIn1.forEach(file => {
-  const dest = join(dir2, file);
-  if (!fs.existsSync(dirname(dest))) {
-    fs.mkdirSync(dirname(dest), { recursive: true });
-  }
-  fs.copyFileSync(join(dir1, file), dest);
+  const dest = path.join(dir2, file);
+  mkdirSyncRecursive(path.dirname(dest));
+  fs.copyFileSync(path.join(dir1, file), dest);
 });
 
 // copy onlyIn2 to dir1, and ensure that the destination directories exist
 onlyIn2.forEach(file => {
-  const dest = join(dir1, file);
-  if (!fs.existsSync(dirname(dest))) {
-    fs.mkdirSync(dirname(dest), { recursive: true });
-  }
-  fs.copyFileSync(join(dir2, file), dest);
+  const dest = path.join(dir1, file);
+  mkdirSyncRecursive(path.dirname(dest));
+  fs.copyFileSync(path.join(dir2, file), dest);
 });
 
-async function touchFile(path, time=new Date()) {
-  await fs.promises.utimes(path, time, time);
+async function touchFile(filePath, time = new Date()) {
+  await fs.promises.utimes(filePath, time, time);
 }
 
 // update common files in both directories. If one side is more recent, copy it to the other
 common.forEach(file => {
   const time = new Date();
-  const src1 = join(dir1, file);
-  const src2 = join(dir2, file);
-  const src1stat = fs.statSync(src1);
-  const src2stat = fs.statSync(src2);
-  // todo: allow some tolerance
-  if (src1stat.mtimeMs > src2stat.mtimeMs) {
+  const src1 = path.join(dir1, file);
+  const src2 = path.join(dir2, file);
+  const src1Stat = fs.statSync(src1);
+  const src2Stat = fs.statSync(src2);
+  if (src1Stat.mtimeMs > src2Stat.mtimeMs) {
     fs.copyFileSync(src1, src2);
     touchFile(src1, time);
     touchFile(src2, time);
-  } else if (src2stat.mtimeMs > src1stat.mtimeMs) {
+  } else if (src2Stat.mtimeMs > src1Stat.mtimeMs) {
     fs.copyFileSync(src2, src1);
     touchFile(src1, time);
     touchFile(src2, time);
