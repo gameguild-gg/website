9c481f941e39ece0090bd0b96a14ae0f6294ad1a
Alec Santos <alecsantos96@gmail.com>
2024-04-07 22:22:52 -0300

    chore: footer clean up

--- services/web/src/components/others/common/footer.tsx
+++ services/web/src/components/others/common/footer.tsx
@@ -1,47 +1,38 @@
 "use client";
-import { Col, Flex, Layout, Row } from "antd";
+
 
 function Footer() {
   return (
-    <Layout.Footer
-      style={{
-        textAlign: "center",
-        color: "#fff",
-        backgroundColor: "#2a2a2a"
-      }}
-    >
-      <Flex justify="center" style={{ width: "100%" }} align="middle">
-        <Row
-          justify="space-between"
-          style={{
-            maxWidth: "1440px",
-            width: "100%",
-            display: "inline-flex",
-            alignItems: "center",
-            alignContent: "center"
-          }}
-        >
-          <Col style={{ display: "inline-flex" }}>
+    <div className="text-white text-center bg-[#2a2a2a]">
+      <div className="flex justify-center w-full align-middle">
+        <div className="justify-between max-w-[1440px] w-full align-center items-center content-center">
+          <div className="flex">
             <a href="#">
               <img
-                style={{ width: 30, height: 30, margin: 3 }}
                 src="assets/images/whatsapp-icon.svg"
+                className="w-[30px] h-[30px] m-3"
               />
             </a>
             <a href="#">
               <img
-                style={{ width: 30, height: 30, margin: 3 }}
                 src="assets/images/discord-icon.svg"
+                className="w-[30px] h-[30px] m-3"
               />
             </a>
-          </Col>
-          <Col>Game Guild © 2024 All Rights Reserved</Col>
-          <Col><a href="#" style={{ color: "white" }}>Privacy Policy</a> | <a href="#" style={{ color: "white" }}>Terms
-            of Service</a></Col>
-        </Row>
-
-      </Flex>
-    </Layout.Footer>
+          </div>
+          <div>Game Guild © 2024 All Rights Reserved</div>
+          <div>
+            <a href="#">
+              Privacy Policy
+            </a>
+            {" | "}
+            <a href="#">
+              Terms of Service
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
