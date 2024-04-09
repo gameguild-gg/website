30284aa6c3332b1c16311b2542d3f9f157fd0d7b
Matheus Martins <contact@matheusmartins.com>
2024-04-09 15:41:39 -0300

    fix: blog layout

--- services/web/src/app/[locale]/blog/layout.tsx
+++ services/web/src/app/[locale]/blog/layout.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default async function Layout({ children }: Readonly<Props>) {
-  return { children };
+  return (children);
 }
