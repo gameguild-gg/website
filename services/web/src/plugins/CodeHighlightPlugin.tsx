03e8a574f1aff7fa490a375e7820df71307a0735
Alexandre Tolstenko <tolstenko@gmail.com>
2024-04-22 22:49:18 -0400

    fix: front without lexical

--- services/web/src/plugins/CodeHighlightPlugin.tsx
+++ services/web/src/plugins/CodeHighlightPlugin.tsx
@@ -1,13 +1,13 @@
-"use client";
-
-import { registerCodeHighlighting } from "@lexical/code";
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { useEffect } from "react";
-
-export default function CodeHighlightPlugin() {
-  const [editor] = useLexicalComposerContext();
-  useEffect(() => {
-    return registerCodeHighlighting(editor);
-  }, [editor]);
-  return null;
-}
+// "use client";
+//
+// import { registerCodeHighlighting } from "@lexical/code";
+// import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+// import { useEffect } from "react";
+//
+// export default function CodeHighlightPlugin() {
+//   const [editor] = useLexicalComposerContext();
+//   useEffect(() => {
+//     return registerCodeHighlighting(editor);
+//   }, [editor]);
+//   return null;
+// }
--- services/web/src/plugins/TreeViewPlugin.tsx
+++ services/web/src/plugins/TreeViewPlugin.tsx
@@ -1,17 +1,17 @@
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { TreeView } from "@lexical/react/LexicalTreeView";
-
-export default function TreeViewPlugin() {
-  const [editor] = useLexicalComposerContext();
-  return (
-    <TreeView
-      viewClassName="tree-view-output"
-      treeTypeButtonClassName="debug-tree-type-button"
-      timeTravelPanelClassName="debug-timetravel-panel"
-      timeTravelButtonClassName="debug-timetravel-button"
-      timeTravelPanelSliderClassName="debug-timetravel-panel-slider"
-      timeTravelPanelButtonClassName="debug-timetravel-panel-button"
-      editor={editor}
-    />
-  );
-}
+// import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+// import { TreeView } from "@lexical/react/LexicalTreeView";
+//
+// export default function TreeViewPlugin() {
+//   const [editor] = useLexicalComposerContext();
+//   return (
+//     <TreeView
+//       viewClassName="tree-view-output"
+//       treeTypeButtonClassName="debug-tree-type-button"
+//       timeTravelPanelClassName="debug-timetravel-panel"
+//       timeTravelButtonClassName="debug-timetravel-button"
+//       timeTravelPanelSliderClassName="debug-timetravel-panel-slider"
+//       timeTravelPanelButtonClassName="debug-timetravel-panel-button"
+//       editor={editor}
+//     />
+//   );
+// }
