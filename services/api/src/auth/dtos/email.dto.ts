d7b56c6f52c2937453a5c0d5bd62ae1adbce011c
Alexandre Tolstenko <tolstenko@gmail.com>
2024-06-26 18:02:57 -0400

    fix: email.dto

--- services/api/src/auth/dtos/email.dto.ts
+++ services/api/src/auth/dtos/email.dto.ts
@@ -1,8 +1,10 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class EmailDto {
   @IsString({ message: 'Email must be a string' })
   @IsEmail({}, { message: 'Email must be a valid email' })
   @IsNotEmpty({ message: 'Email must not be empty' })
+  @ApiProperty()
   email: string;
 }
