72e8998c671f2ee1f69ccaa03756a084524853a7
Matheus Martins <contact@matheusmartins.com>
2024-09-15 16:35:32 -0300

    fix: apinest.ts

--- services/web/src/lib/apinest.ts
+++ services/web/src/lib/apinest.ts
@@ -1,8 +1,8 @@
 import { AuthApi, CompetitionsApi, Configuration } from '@game-guild/apiclient';
 
 const configuration = new Configuration({
-  // basePath: process.env.NEST_JS_BACKEND_URL,
-  basePath: 'http://localhost:4000',
+  basePath: process.env.BACKEND_URL,
+  // basePath: 'http://localhost:4000',
 });
 
 export const competitionsApi = new CompetitionsApi(configuration);
