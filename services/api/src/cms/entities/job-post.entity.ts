cc35625070a6aaadfc98eafa23da390a8170b7a2
Alec Santos <alecsantos96@gmail.com>
2024-10-02 21:16:42 -0300

    feat: Job typeorm initial structure

---
+++ services/api/src/cms/entities/job-post.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, JoinTable, OneToMany } from 'typeorm';
+import { ContentBase } from './content.base';
+import { JobTagEntity } from './job-tag.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
+@Entity({ name: 'job-post' })
+export class JobPostEntity extends ContentBase {
+  // a job is very similar to a content base, but has many job tags
+  @OneToMany(() => JobTagEntity, (jobTag) => jobTag.job)
+  @ApiProperty({ type: JobTagEntity, isArray: true })
+  @IsArray({ message: 'error.IsArray: lectures should be an array' })
+  @ValidateNested({ each: true })
+  @Type(() => JobTagEntity)
+  @JoinTable()
+  tags: JobTagEntity[];
+}
