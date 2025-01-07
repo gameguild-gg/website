cdd07d2592034e814591e729daf4600e3f01d829
Alexandre Tolstenko Nogueira <tolstenko@gmail.com>
2025-01-07 12:41:31 -0500

    fix(api): fix minor typo preventing api build

--- services/api/src/cms/courses.service.ts
+++ services/api/src/cms/courses.service.ts
@@ -1,10 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { CourseEntity } from './entities/Course.entity';
+import { CourseEntity } from './entities/course.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { WithRolesService } from './with-roles.service';
-//import { Api } from '@game-guild/apiclient';
-//import CourseVersionEntity = Api.CourseVersionEntity;
 
 @Injectable()
 export class CourseService extends WithRolesService<CourseEntity> {
