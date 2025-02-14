a3c58817bce0d53e2b599f5fc798017e6257c28c
Matheus Martins <contact@matheusmartins.com>
2025-02-14 17:24:38 -0300

    fix(code activity): fix code activity dynamic scaling

--- services/web/src/components/markdown-renderer/MarkdownCodeActivity.tsx
+++ services/web/src/components/markdown-renderer/MarkdownCodeActivity.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { CodeLanguage, useWasmer, WasmerStatus } from '@/components/wasmer/use-wasmer';
 import { Card } from '@/components/ui/card';
-import Editor from '@monaco-editor/react';
+import Editor, { OnMount } from '@monaco-editor/react';
 import { Play } from 'lucide-react';
-import MarkdownRenderer from '@/components/markdown-renderer/markdown-renderer';
 
 export interface MarkdownCodeActivityProps {
   code: string;
@@ -20,13 +19,21 @@ export function MarkdownCodeActivity(params: MarkdownCodeActivityProps) {
   const { wasmerStatus, runCode, error } = useWasmer();
   const [stdErr, setStdErr] = useState<string>('');
   const [stdOut, setStdOut] = useState<string>('');
-  const [code, setCode] = useState<string>('');
+  const [code, setCode] = useState<string>(params.code);
   const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
 
-  // set code only on first render
-  useEffect(() => {
-    setCode(params.code);
-  }, []);
+  const onEditorDidMount: OnMount = (editor) => {
+    const updateHeight = () => {
+      const contentHeight = editor.getContentHeight(); //Math.min(1000, editor.getContentHeight());
+      const currentWidth = editor.getLayoutInfo().width;
+
+      editor.getContentHeight();
+      editor.layout({ width: currentWidth, height: contentHeight });
+    };
+    editor.onDidContentSizeChange(updateHeight);
+
+    updateHeight();
+  };
 
   const handleRunCode = async () => {
     if (wasmerStatus == WasmerStatus.RUNNING || wasmerStatus == WasmerStatus.LOADING_PACKAGE || wasmerStatus == WasmerStatus.FAILED_LOADING_WASMER) return;
@@ -50,39 +57,40 @@ export function MarkdownCodeActivity(params: MarkdownCodeActivityProps) {
     } else setIsCorrect(true);
   };
 
-  const numberOfLines = code.split('\n').length;
-
   return (
-    <div className="container mx-auto p-4 space-y-4">
-      <MarkdownRenderer content={params.description} />
-      {/* Editor de código */}
+    <Card className="container flex flex-auto flex-col p-4 gap-4 shadow-lg border border-gray-300">
+      <p className="text-lg font-bold">{params.description}</p>
       <Card className="bg-[#1e1e1e] text-white p-4 font-mono text-sm">
         <Editor
-          height={`${params.height || Math.floor(numberOfLines * 21)}px`}
+          // height={`${params.height || Math.floor(numberOfLines * 21)}px`}
           defaultLanguage={params.language}
           theme="vs-dark"
           value={code}
           onChange={(value) => setCode(value || '')}
+          height={'100%'}
+          width={'100%'}
+          onMount={onEditorDidMount}
           options={{
             minimap: { enabled: false },
             fontSize: 14,
             lineNumbers: 'on',
             readOnly: isCorrect === true,
             domReadOnly: false,
-            padding: { top: 0 },
+            padding: { top: 0, bottom: 0 },
             scrollBeyondLastLine: false,
             automaticLayout: true,
-            lineNumbersMinChars: Math.floor(Math.log10(numberOfLines) + 1),
           }}
         />
       </Card>
 
-      {isCorrect === true && <div className={`p-2 rounded bg-red-100 text-green-800`}>Correct output!</div>}
+      {isCorrect === true && (
+        <div className="p-3 rounded-md bg-green-100 border border-green-300 text-green-800 font-semibold text-center">Correct output!</div>
+      )}
 
       {/* Área de saída */}
       {wasmerStatus != WasmerStatus.UNINITIALIZED && !(wasmerStatus == WasmerStatus.READY_TO_RUN && isCorrect) && (
         <>
-          <Card className="bg-[#2d2d2d] text-white p-4 min-h-[150px] font-mono">
+          <Card className="bg-[#2d2d2d] text-white p-4 min-h-fit font-mono">
             {wasmerStatus == WasmerStatus.LOADING_WASMER && <p>Loading Wasmer...</p>}
             {wasmerStatus == WasmerStatus.LOADING_PACKAGE && <p>Loading Python...</p>}
             {wasmerStatus == WasmerStatus.RUNNING && <p>Running...</p>}
@@ -116,6 +124,6 @@ export function MarkdownCodeActivity(params: MarkdownCodeActivityProps) {
           </Button>
         </div>
       )}
-    </div>
+    </Card>
   );
 }
