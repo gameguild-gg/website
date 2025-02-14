a5b0200cf4169097b4ff9a1b16139964bebf63f5
Alexandre Tolstenko Nogueira <tolstenko@gmail.com>
2025-02-14 10:24:20 -0500

    fix(courses): typo

--- services/web/src/data/courses/ai4games/chapters/week05/index.ts
+++ services/web/src/data/courses/ai4games/chapters/week05/index.ts
@@ -1,14 +1,15 @@
 import { Api } from '@game-guild/apiclient';
 import { createChapter, createLecture } from '@/data/coursesLib';
 import lecture from './lecture.md';
+import lecture2 from './lecture2.md';
 import ChapterEntity = Api.ChapterEntity;
 import LectureEntity = Api.LectureEntity;
 
 const week05lectures: LectureEntity[] = [];
 
-week05lectures.push(createLecture('5-1', 'dynamic-goap', 'Dynamic GOAP in C++', 'Modifying GOAP plans at runtime in C++', lecture, 1) as LectureEntity);
+week05lectures.push(createLecture('5-1', 'dynamic-goap-cpp', 'Dynamic GOAP in C++', 'Modifying GOAP plans at runtime in C++', lecture, 1) as LectureEntity);
 
-week05lectures.push(createLecture('5-2', 'dynamic-goap-cs', 'Dynamic GOAP in C#', 'Modifying GOAP plans at runtime in C#', lecture, 1) as LectureEntity);
+week05lectures.push(createLecture('5-2', 'dynamic-goap-cs', 'Dynamic GOAP in C#', 'Modifying GOAP plans at runtime in C#', lecture2, 2) as LectureEntity);
 
 const Chapter05 = createChapter('5', 'week05', 'Week 5: Dynamic GOAP', 'Dynamic GOAP.', 5, ['5-1', '5-2'], week05lectures) as ChapterEntity;
 
