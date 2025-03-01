29b713ccb170aeae9e623856c945420418071f68
Matheus Martins <contact@matheusmartins.com>
2025-03-01 15:31:21 -0300

    chore(apps/api): Added api ap 2e2 test

---
+++ apps/api/test/app.e2e-spec.ts
@@ -0,0 +1,25 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('should redirect to /documentation with 301 status', () => {
+    return request(app.getHttpServer()).get('/').expect(HttpStatus.MOVED_PERMANENTLY).expect('Location', '/documentation');
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+});
