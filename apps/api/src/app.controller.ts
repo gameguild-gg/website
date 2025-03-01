402760483078baffb3677dbcc5fc77aad3423537
Matheus Martins <contact@matheusmartins.com>
2025-03-01 15:32:19 -0300

    chore(apps/api): Refactored the app controller

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,10 @@
-import { Controller, Get, Redirect } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Redirect } from '@nestjs/common';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
-import { Auth, PublicRoute } from './auth';
 
 @Controller()
 export class AppController {
   @Get()
-  @Redirect('/documentation', 302)
+  @Redirect('/documentation', HttpStatus.MOVED_PERMANENTLY)
   @ApiExcludeEndpoint()
-  // @Auth(PublicRoute)
-  root() {}
+  private async root() {}
 }
