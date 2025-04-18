c4e39306b51d6ade26b29731712e41798468393d
Matheus Martins <contact@matheusmartins.com>
2025-04-18 13:49:39 -0300

    removed some boilerplate projects

--- apps/courses/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import SyntaxTraining from "./components/SyntaxTraining";
-import React from "react";
-export default function App() {
-  return (
-    <main>
-      <SyntaxTraining />
-    </main>
-  );
-}
