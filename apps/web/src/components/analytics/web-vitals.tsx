00dd2c0049faba91b1460340cdae5f98d53c0511
Matheus Martins <contact@matheusmartins.com>
2025-04-20 16:43:36 -0300

    feat(apps/web): Send Web Vitals to Goggle Analytics as custom metrics

--- apps/web/src/components/analytics/web-vitals.tsx
+++ apps/web/src/components/analytics/web-vitals.tsx
@@ -1,18 +1,41 @@
 'use client';
 
 import { useReportWebVitals } from 'next/web-vitals';
+import { sendGAEvent } from '@next/third-parties/google';
 
-export function WebVitals() {
+export const WebVitals = (): null => {
   useReportWebVitals((metric) => {
+    const payload = JSON.stringify(metric);
+
     // Try to send the metric to an analytics endpoint
     // using `navigator.sendBeacon` if available.
-    // if (navigator.sendBeacon){
-    //
+    // TODO: Uncomment this when the endpoint is ready.
+    // if (navigator.sendBeacon) {
+    //   navigator.sendBeacon('/api/web‑vitals', payload);
     // } else {
-    // TODO: Fallback to `fetch` if `navigator.sendBeacon` is not available.
+    //   void fetch('/api/web‑vitals', {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: payload,
+    //     keepalive: true,
+    //   });
     // }
-    // TODO: https://github.com/GoogleChrome/web-vitals#send-the-results-to-google-analytics
+
+    sendGAEvent('event', metric.name, {
+      value: Math.round(metric.name === 'CLS' ? metric.value * 1000 : metric.value),
+      metric_id: metric.id,
+      metric_category: 'Web Vitals',
+      metric_value: metric.value,
+      metric_delta: metric.delta,
+      metric_rating: metric.rating,
+      metric_navigation_type: metric.navigationType,
+      page_path: window.location.pathname,
+      non_interaction: true,
+    });
+
+    // TODO: Remove this console log in production
+    console.log('Web Vitals metric:', metric);
   });
 
   return null;
-}
+};
