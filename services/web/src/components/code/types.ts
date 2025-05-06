dbb7df96a90d165ba03492902d219716803cf8d3
Alexandre Tolstenko Nogueira <tolstenko@gmail.com>
2025-05-06 18:09:44 -0400

    fix(code): code test type

--- services/web/src/components/code/types.ts
+++ services/web/src/components/code/types.ts
@@ -22,7 +22,7 @@ export type SimpleCodingTest = {
   stdin: string;
 } & SimpleCodingOutputOnly;
 
-/* function testing: in / out test */
+/* function testing: args / result test */
 export type FunctionCodingTest = {
   // the arguments to be passed to the function
   args: any[];
@@ -33,7 +33,7 @@ export type FunctionCodingTest = {
 export type FunctionCodingTests = {
   publicTests: FunctionCodingTest[];
   hiddenTests: FunctionCodingTest[];
-}
+};
 
 /* the instructor can define their own tests */
 export type InstructorDefinedTest = {
@@ -41,11 +41,11 @@ export type InstructorDefinedTest = {
   // useful for testing time complexity
   // the instructor can implement their own solution for benchmarking and then compare the time against the user's solution
   testerSourceCode: string;
-}
+};
 export type InstructorDefinedTestResultEntry = {
   // number between 0 and 1 for each individual test
   grade: number;
-  // if the test pass, there is no need to show message 
+  // if the test pass, there is no need to show message
   message?: string;
 };
 export type InstructorDefinedTestResult = {
@@ -53,36 +53,16 @@ export type InstructorDefinedTestResult = {
   grade: number;
   // verbose message to be shown to the user
   results?: InstructorDefinedTestResultEntry[];
-}
+};
 export type InstructorDefinedTests = {
   publicTests: InstructorDefinedTest[];
   hiddenTests: InstructorDefinedTest[];
 };
 
-
-// while testing complexity, we need to test the code with different sizes and then do some regression on the samples to infer the complexity
-export type CodeComplexityTestEntry = SimpleCodingTest & {
-  // used to determine the complexity in the regression algorithm
-  // the first entry usually is 1 and the others should be proportional to the size of the input
-  time: number;
-
-  // the size of the input
-  // probably not needed, but it could be useful to determine the complexity
-  n: number;
-};
-
-export type CodeComplexityTest = {
-  entries: CodeComplexityTestEntry[];
-
-  // acceptable error variance when comparing the relative time results against the expected time
-  // the lower, the more accurate the results
-  tolerance: number;
-};
-
 export type CodingTestParams = {
   // it could be just a string for a file or a filesystem
   files: Directory | FileMap | string;
-  tests: SimpleCodingOutputOnly | SimpleCodingTest[] | CodeComplexityTestEntry[];
+  tests: SimpleCodingOutputOnly | SimpleCodingTest[] | InstructorDefinedTests[];
 };
 
 export enum CodeComplexity {
