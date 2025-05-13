b188aba7c3e6daf741f3026e54d027b4b4921872
Alexandre Tolstenko Nogueira <tolstenko@gmail.com>
2025-05-13 15:18:02 -0400

    fix: institutional page routing. closes #101

--- services/web/src/components/header/index.tsx
+++ services/web/src/components/header/index.tsx
@@ -13,15 +13,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import {
-  Bell,
-  ChevronDown,
-  ChevronUp,
-  Globe,
-  Menu,
-  Search,
-  X,
-} from 'lucide-react';
+import { Bell, ChevronDown, ChevronUp, Globe, Menu, Search, X } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 
 /**
@@ -35,18 +27,7 @@ export default function Header() {
   const [isComplexOpen, setIsComplexOpen] = useState(false);
 
   const menuItems = ['Blog', 'Courses']; // ToDo: Jams, Events, Jobs, Tests, Projects
-  const moreItems = [
-    'Roadmap',
-    'Contributors',
-    'Issues',
-    'Code of Conduct',
-    'Security',
-    'Privacy Policy',
-    'Terms of Service',
-    'Contact',
-    'About',
-    'License',
-  ];
+  const moreItems = ['Roadmap', 'Contributors', 'Issues', 'Code of Conduct', 'Security', 'Privacy Policy', 'Terms of Service', 'Contact', 'About', 'License'];
 
   const complexItems = [
     { name: 'Learn', href: `/learn` },
@@ -85,56 +66,34 @@ export default function Header() {
     <header className="bg-neutral-900 h-[70px] flex items-center justify-between px-4 text-white">
       <div className="flex items-center">
         <Link href="/" className="mr-4">
-          <img
-            src="/assets/images/logo-text.png"
-            className="h-[40px] my-auto mx-[10px]"
-          />
+          <img src="/assets/images/logo-text.png" className="h-[40px] my-auto mx-[10px]" />
         </Link>
         <nav className="hidden md:flex space-x-2 h-[50px] p-0">
           {menuItems.map((item) => (
-            <button
-              key={item}
-              onClick={() => router.push(`/${item.toLowerCase()}`)}
-              className="px-2 my-0 hover:text-gray-400 transition-colors h-full"
-            >
+            <button key={item} onClick={() => router.push(`/${item.toLowerCase()}`)} className="px-2 my-0 hover:text-gray-400 transition-colors h-full">
               {item}
             </button>
           ))}
           <DropdownMenu open={isMoreOpen} onOpenChange={setIsMoreOpen}>
             <DropdownMenuTrigger className="flex px-2 items-center hover:text-gray-400 transition-colors">
-              Institutional{' '}
-              {isMoreOpen ? (
-                <ChevronUp className="ml-1 h-4 w-4" />
-              ) : (
-                <ChevronDown className="ml-1 h-4 w-4" />
-              )}
+              Institutional {isMoreOpen ? <ChevronUp className="ml-1 h-4 w-4" /> : <ChevronDown className="ml-1 h-4 w-4" />}
             </DropdownMenuTrigger>
             <DropdownMenuContent className="bg-neutral-900 text-white border-0">
               {moreItems.map((item) => (
-                <DropdownMenuItem key={item}>
-                  <Link href={`/` + item.toLowerCase().replace(/\s/g, '')}>
-                    {item}
-                  </Link>
-                </DropdownMenuItem>
+                <Link key={item} href={`/` + item.toLowerCase().replace(/\s/g, '')}>
+                  <DropdownMenuItem>{item}</DropdownMenuItem>
+                </Link>
               ))}
             </DropdownMenuContent>
           </DropdownMenu>
           <DropdownMenu open={isComplexOpen} onOpenChange={setIsComplexOpen}>
             <DropdownMenuTrigger className="flex px-2 items-center hover:text-gray-400 transition-colors">
-              Learn{' '}
-              {isMoreOpen ? (
-                <ChevronUp className="ml-1 h-4 w-4" />
-              ) : (
-                <ChevronDown className="ml-1 h-4 w-4" />
-              )}
+              Learn {isMoreOpen ? <ChevronUp className="ml-1 h-4 w-4" /> : <ChevronDown className="ml-1 h-4 w-4" />}
             </DropdownMenuTrigger>
             <DropdownMenuContent className="bg-neutral-900 text-white border-0">
               {complexItems.map((item) => (
                 <DropdownMenuItem key={item.name}>
-                  <Link
-                    href={item.href}
-                    className=" hover:text-gray-400 transition-colors"
-                  >
+                  <Link href={item.href} className=" hover:text-gray-400 transition-colors">
                     {item.name}
                   </Link>
                 </DropdownMenuItem>
@@ -146,24 +105,14 @@ export default function Header() {
       <div className="hidden md:flex items-center space-x-4">
         {isSearchOpen ? (
           <div className="relative text-white w-80">
-            <Input
-              type="text"
-              placeholder="Search..."
-              className="pl-10 pr-10 py-2 rounded-full bg-neutral-800 text-white placeholder:text-gray-200"
-            />
+            <Input type="text" placeholder="Search..." className="pl-10 pr-10 py-2 rounded-full bg-neutral-800 text-white placeholder:text-gray-200" />
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-200" />
-            <button
-              onClick={() => setIsSearchOpen(false)}
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-200 hover:text-white"
-            >
+            <button onClick={() => setIsSearchOpen(false)} className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-200 hover:text-white">
               <X className="h-4 w-4" />
             </button>
           </div>
         ) : (
-          <button
-            onClick={() => setIsSearchOpen(true)}
-            className="hover:text-gray-200 transition-colors"
-          >
+          <button onClick={() => setIsSearchOpen(true)} className="hover:text-gray-200 transition-colors">
             <Search />
           </button>
         )}
@@ -192,15 +141,11 @@ export default function Header() {
                 {/*<img src={user.avatar} alt={user.name[0]} className="w-8 h-8 rounded-full" />*/}
                 <Avatar className="mx-2">
                   <AvatarImage src={user.image} alt={user.name} />
-                  <AvatarFallback className="bg-neutral-50 text-neutral-950">
-                    {user.name[0]}
-                  </AvatarFallback>
+                  <AvatarFallback className="bg-neutral-50 text-neutral-950">{user.name[0]}</AvatarFallback>
                 </Avatar>
               </DropdownMenuTrigger>
               <DropdownMenuContent>
-                <DropdownMenuItem onClick={() => router.push('/profile/edit')}>
-                  Profile
-                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => router.push('/profile/edit')}>Profile</DropdownMenuItem>
                 <DropdownMenuItem>Settings</DropdownMenuItem>
                 <DropdownMenuItem
                   onClick={
@@ -226,21 +171,14 @@ export default function Header() {
         )}
       </div>
       <div className="md:hidden">
-        <button
-          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-          className="text-white items-center"
-        >
+        <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className="text-white items-center">
           {isMobileMenuOpen ? <X size={42} /> : <Menu size={42} />}
         </button>
       </div>
       {isMobileMenuOpen && (
         <div className="absolute top-[70px] left-0 right-0 bg-neutral-900 p-4 md:hidden z-30">
           <div className="flex flex-col space-y-4">
-            <Input
-              type="text"
-              placeholder="Search..."
-              className="pl-10 pr-10 py-2 rounded-full bg-neutral-800 text-white"
-            />
+            <Input type="text" placeholder="Search..." className="pl-10 pr-10 py-2 rounded-full bg-neutral-800 text-white" />
             <DropdownMenu>
               <DropdownMenuTrigger className="flex items-center justify-between w-full">
                 <span>Language</span>
@@ -266,11 +204,7 @@ export default function Header() {
                 </DropdownMenu>
                 <DropdownMenu>
                   <DropdownMenuTrigger className="flex items-center space-x-2">
-                    <img
-                      src={user.avatar}
-                      alt={user.name}
-                      className="w-8 h-8 rounded-full"
-                    />
+                    <img src={user.avatar} alt={user.name} className="w-8 h-8 rounded-full" />
                     <span>{user.name}</span>
                   </DropdownMenuTrigger>
                   <DropdownMenuContent>
@@ -290,11 +224,7 @@ export default function Header() {
               </Button>
             )}
             {menuItems.map((item) => (
-              <Link
-                key={item}
-                href={`/${item.toLowerCase()}`}
-                className="hover:text-gray-400 transition-colors"
-              >
+              <Link key={item} href={`/${item.toLowerCase()}`} className="hover:text-gray-400 transition-colors">
                 {item}
               </Link>
             ))}
